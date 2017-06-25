b2719289e5cb3542e9243dc7d39839aec3f0bda5
Anwesha Das <anwesha@das.community>
2017-06-25 17:27:33 +0530

    Gives details of dates,authors

--- gitcen.py
+++ gitcen.py
@@ -1,5 +1,57 @@
 import click
 import os
+import datetime
+from pygit2 import Repository
+from pygit2 import GIT_SORT_TOPOLOGICAL, GIT_SORT_REVERSE
+
+
+def date(path):
+    data = {}
+    repo = Repository(path)
+    for commit in repo.walk(repo.head.target, GIT_SORT_TOPOLOGICAL):
+        day = datetime.datetime.fromtimestamp(commit.commit_time).strftime('%A')
+        number = data.get(day, 0)
+        number += 1
+        data[day] = number
+    print("Sunday has {0} commits".format(data["Sunday"]))
+    print("Monday has {0} commits".format(data["Monday"]))
+    print("Tuesday has {0} commits".format(data["Tuesday"]))
+    print("Wednesday has {0} commits".format(data["Wednesday"]))
+    print("Thursday has {0} commits".format(data["Thursday"]))
+    print("Friday has {0} commits".format(data["Friday"]))
+    print("Saturday has {0} commits".format(data["Saturday"]))
+
+
+
+
+def time(path):
+    times = {}
+    repo = Repository(path)
+    for commit in repo.walk(repo.head.target, GIT_SORT_TOPOLOGICAL):
+        time = datetime.datetime.fromtimestamp(commit.commit_time).strftime('%-H')
+        number = times.get(time, 0)
+        number += 1
+        times[time] = number
+    for time in range(0, 24):
+        print("%d has %d commits" % (time, times.get(str(time), 0)))
+
+def authors(path):
+    info = {}
+    repo = Repository(path)
+    for commit in repo.walk(repo.head.target, GIT_SORT_TOPOLOGICAL):
+        author = commit.author.name
+        email = commit.author.email
+        key = "{0} <{1}>".format(author, email)
+        number = info.get(key, 0)
+        number += 1
+        info[key] = number
+    for author, number in info.items():
+        msg = "{0} has {1} commits".format(author, number)
+        print(msg)
+
+
+
+
 
 @click.command()
 @click.option('--path',help="Will print the path provided.")
@@ -15,7 +67,9 @@ def main(path):
 
         if not path.endswith(".git" or ".git/"):
             path = os.path.join(path, ".git")
-        print(path)
+            if os.path.exists(path):
+                date(path)
+                authors(path)
     else:
         print("Please provide a correct git repository path.")
 
