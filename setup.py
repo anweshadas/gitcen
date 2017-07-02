9d3c3e83df0c2ae5fd11638bdb3af3292198b117
Anwesha Das <anwesha@das.community>
2017-07-02 22:16:40 +0530

    Updates setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import setup
 setup(
     name="gitcen",
     version='0.1',
+    description="A project to find git information.",
+    long_description="A project to find git information about authors' commits,most active days and time.",
+    author="Anwesha Das.",
+    author_email="anwesha@das.community",
+    url="https://github.com/anweshadas/gitcen",
+    license="GPLv3+",
     py_modules=['gitcen'],
     install_requires=[
         'Click',
@@ -11,4 +17,11 @@ setup(
         [console_scripts]
         gitcen=gitcen:main
     ''',
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Environment:: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language:: Python:: 3.6'
+    )
 )
