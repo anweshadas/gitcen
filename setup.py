1bf62c3d8348a293a5c09faf077fe992560f440b
Anwesha Das <anwesha@das.community>
2017-07-03 16:39:06 +0530

    Corrects the value for programming language

--- setup.py
+++ setup.py
@@ -22,6 +22,6 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Programming Language:: Python:: 3.6'
+        'Programming Language :: Python :: 3.6'
     )
 )
